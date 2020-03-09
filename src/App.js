57e2b31babb3f6cbde632b827a0c490078532abc
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-09 19:06:55 +0300

    feat: add Footer import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Footer from './components/Footer/Footer'
 
 const App = () => (
     <div className="app-wrapper">
-        qq
+        <Footer />
     </div>
 );
 
