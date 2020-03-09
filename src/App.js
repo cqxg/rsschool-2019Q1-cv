1e267893aaf02717add326ba1900d76b4794240c
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-09 19:07:48 +0300

    feat: add Header import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 
 const App = () => (
     <div className="app-wrapper">
         <Footer />
+        <Header />
+
     </div>
 );
 
