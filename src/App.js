2ecb1597f254239672d581583fa0cb90dd0367c4
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-09 19:12:09 +0300

    feat: add main styles folder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LeftCol from './components/LeftCol/LeftCol'
 import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
 
+import './Styles/styles.css'
 
 const App = () => (
     <div className="app-wrapper">
