19ccb5ad4d1a681bfcf0bcc9c2848f1007042eb9
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-10 20:23:05 +0300

    fix: header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,37 @@
 import React from 'react';
 
-const Header = () => (
-  <div>
-    <div id="header">
-      <div className="header_logotype_container">
-        <h1 className="logotype_name">
-                    Ihar
-          <span className="purple">Ivaniuk</span>
-        </h1>
-        <h2 className="logotype_occupation">Web Developer</h2>
-      </div>
-      <div className="header_menu_container">
-        <ul className="download_print_buttons horizontal_list">
-          <li>
-            <a href="./assets/Ihar_Ivaniuk_CV.pdf" download>
-              <span className="icon entypo-download" />
-              Download CV
-            </a>
-          </li>
-          <li>
-            <a className="print" href="#">
-              <span className="icon entypo-print" />
-Print CV
-            </a>
-          </li>
-        </ul>
-        <div className="clear" />
-        <ul className="header_menu horizontal_list">
-          <li><a className="no_border purple" href="#">Profile</a></li>
-          <li><a href="https://github.com/cqxg">Github</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-);
+const Header = () => {
+
+    const goPrint = () => {
+        window.print();
+    };
+
+    return (
+        <div>
+            <div id="header">
+                <div className="header_logotype_container">
+                    <h1 className="logotype_name">Ihar<span className="purple"> Ivaniuk</span>
+                    </h1>
+                    <h2 className="logotype_occupation">Web Developer</h2>
+                </div>
+                <div className="header_menu_container">
+                    <ul className="download_print_buttons horizontal_list">
+                        <li>
+                            <a href="./assets/Ihar_Ivaniuk_CV.pdf" download><span className="icon entypo-download" />Download CV</a>
+                        </li>
+                        <li onClick={() => goPrint()}>
+                            <a className="print" href="#"><span className="icon entypo-print" />Print CV</a>
+                        </li>
+                    </ul>
+                    <div className="clear" />
+                    <ul className="header_menu horizontal_list">
+                        <li><a className="no_border purple" href="#">Profile</a></li>
+                        <li><a href="https://github.com/cqxg">Github</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Header;
