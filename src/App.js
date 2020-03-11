c02ee1417a11517e584f1c6bbf9cc899fd0b2f3c
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-11 19:58:28 +0300

    feat: add Profile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import LeftCol from './components/LeftCol/LeftCol';
