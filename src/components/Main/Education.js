c4889a85e53cde1c911418633a112df92de00c4a
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-13 22:48:05 +0300

    fix: lint:fix

--- src/components/Main/Education.js
+++ src/components/Main/Education.js
@@ -1,23 +1,38 @@
 import React from 'react';
 
 const Education = () => (
-    <div id="content_container">
-        <div class="block">
-            <h2>Education</h2>
-            <p>The Rolling Scopes School <a class="education_link"
-                href="https://school.rollingscopes.com/">(school.rollingscopes)</a>, 02.2019 – 08.2019</p>
-            <p>Certificate of Completion:</p>
-            <a href="https://app.rs.school/certificate/cc6pbvqf"><img src="./assets//Certificate.png" alt="" /></a>
+  <div id="content_container">
+    <div className="block">
+      <h2>Education</h2>
+      <p>
+The Rolling Scopes School
+        <a
+          className="education_link"
+          href="https://school.rollingscopes.com/"
+        >
+(school.rollingscopes)
+        </a>
+, 02.2019 – 08.2019
+      </p>
+      <p>Certificate of Completion:</p>
+      <a href="https://app.rs.school/certificate/cc6pbvqf"><img src="./assets//Certificate.png" alt="" /></a>
 
-            <div class="horizontal_line">
-                <div class="line_left"></div>
-            </div>
+      <div className="horizontal_line">
+        <div className="line_left" />
+      </div>
 
-            <p>Belarusian State Economic University<a class="education_link" href="http://bseu.by/english/"> (Minsk,
-                        Belarus)</a>, 2014-2018</p>
-            <p>Specialist (Bachelor Degree) – Political science.</p>
-        </div>
+      <p>
+Belarusian State Economic University
+        <a className="education_link" href="http://bseu.by/english/">
+          {' '}
+(Minsk,
+                        Belarus)
+        </a>
+, 2014-2018
+      </p>
+      <p>Specialist (Bachelor Degree) – Political science.</p>
     </div>
+  </div>
 );
 
 export default Education;
--- src/components/Main/Profile.js
+++ src/components/Main/Profile.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 const Profile = () => (
     <div id="content_container">
-        <div class="block">
+        <div className="block">
             <h1>Summary of Qualifications</h1>
-            <blockquote class="profile_quote">
+            <blockquote className="profile_quote">
                 <p>Junior frontend-developer.</p>
                 <p>
                     Eager to get working experience and to become a true professional in web development.
                     Confident knowledge of HTML, CSS, JavaScript, React, Redux, Git and some other technology
-                    </p>
+        </p>
             </blockquote>
         </div>
-        <div class="horizontal_line">
-            <div class="line_left"></div>
-            <div class="left_circle"></div>
-            <div class="central_circle"></div>
-            <div class="right_circle"></div>
-            <div class="line_right"></div>
+        <div className="horizontal_line">
+            <div className="line_left" />
+            <div className="left_circle" />
+            <div className="central_circle" />
+            <div className="right_circle" />
+            <div className="line_right" />
         </div>
-        <div class="block">
+        <div className="block">
             <h2>Technical Skills</h2>
-            <div class="philosophy_content">
+            <div className="philosophy_content">
                 <p>Programming languages and technologies:</p>
                 <ul>
                     <li>JavaScript, HTML, CSS</li>
@@ -46,7 +46,7 @@ const Profile = () => (
                 <ul>
                     <li>Agile, Kanban, Code review</li>
                 </ul>
-                <div class="clear"></div>
+                <div className="clear" />
             </div>
         </div>
     </div>
