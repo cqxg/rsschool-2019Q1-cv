61b3d28afdb7d20c84c96a000b5fe65bb5d019ec
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-14 19:40:58 +0300

    fix: profile color

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <nav>
     <ul className="header_menu horizontal_list">
-      <li><NavLink to="/profile" class="no_border purple">Profile</NavLink></li>
+      <li><NavLink to="/profile">Profile</NavLink></li>
       <li><NavLink to="/education" href="https://github.com/cqxg">Education</NavLink></li>
       <li><NavLink to="/projects" href="https://github.com/cqxg">Projects</NavLink></li>
     </ul>
