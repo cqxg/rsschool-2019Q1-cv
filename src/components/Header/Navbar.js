99a138374e1f41a5b11d8c4833c410529586d4df
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-14 19:45:17 +0300

    feat: unnecessary links from nav removed

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => (
   <nav>
     <ul className="header_menu horizontal_list">
       <li><NavLink to="/profile" activeClassName='active'>Profile</NavLink></li>
-      <li><NavLink to="/education" activeClassName='active' href="https://github.com/cqxg">Education</NavLink></li>
-      <li><NavLink to="/projects" activeClassName='active' href="https://github.com/cqxg">Projects</NavLink></li>
+      <li><NavLink to="/education" activeClassName='active'>Education</NavLink></li>
+      <li><NavLink to="/projects" activeClassName='active'>Projects</NavLink></li>
     </ul>
   </nav>
 );
