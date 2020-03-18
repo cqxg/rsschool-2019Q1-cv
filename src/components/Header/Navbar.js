b691e73902f5f6ab0b5c7d7fe68824a043926996
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-18 20:06:23 +0300

    fix: navbar ident

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav>
-    <ul className="header_menu horizontal_list">
-      <li><NavLink to="/profile" activeClassName='active'>Profile</NavLink></li>
-      <li><NavLink to="/education" activeClassName='active'>Education</NavLink></li>
-      <li><NavLink to="/projects" activeClassName='active'>Projects</NavLink></li>
-    </ul>
-  </nav>
+    <nav>
+        <ul className="header_menu horizontal_list">
+            <li><NavLink to="/profile" activeClassName='active'>Profile</NavLink></li>
+            <li><NavLink to="/education" activeClassName='active'>Education</NavLink></li>
+            <li><NavLink to="/projects" activeClassName='active'>Projects</NavLink></li>
+        </ul>
+    </nav>
 );
 
 export default Navbar;
