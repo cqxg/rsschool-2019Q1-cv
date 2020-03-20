01c7d579dc74e0eb4ad0613608313877412b3c5f
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-20 20:57:46 +0300

    fix: rss education link

--- src/components/Main/Education.js
+++ src/components/Main/Education.js
@@ -8,7 +8,7 @@ const Education = () => (
             <h2>Education</h2>
             <a href="http://bseu.by/english/" target='_blank'><img src={University} alt="" /></a>
             <p>Belarusian State Economic University
-                <a className="education_link" href="http://bseu.by/english/">{' '}(Minsk,Belarus)</a>
+                <a className="education_link" target='_blank' href="http://bseu.by/english/">{' '}(Minsk,Belarus)</a>
                 , 2014-2018</p>
             <p>Specialist (Bachelor Degree) – Political science.</p>
 
@@ -16,11 +16,11 @@ const Education = () => (
                 <div className="line_left" />
             </div>
 
-            <p>The Rolling Scopes School<a className="education_link" href="https://school.rollingscopes.com/">
+            <p>The Rolling Scopes School<a className="education_link" target='_blank' href="https://school.rollingscopes.com/">
                 (school.rollingscopes)</a>, 02.2019 – 08.2019</p>
 
             <p>Certificate of Completion:</p>
-            <a href="https://app.rs.school/certificate/cc6pbvqf"><img src={Certificate} alt="" /></a>
+            <a href="https://app.rs.school/certificate/cc6pbvqf" target='_blank'><img src={Certificate} alt="" /></a>
         </div>
     </div>
 );
