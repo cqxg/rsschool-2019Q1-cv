517c85bcd624829444b4a1faf182ed4c4361b639
Ihar Ivaniuk <cqxg@yandex.ru>
2020-03-20 20:54:12 +0300

    fix: footer ident

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Footer = () => (
-  <>
-    <div className="clear" />
-    <div id="footer">
-      <p className="footer_name">Ihar Ivaniuk aka cqxg CV 2020</p>
+    <div>
+        <div className="clear" />
+        <div id="footer">
+            <p className="footer_name">Ihar Ivaniuk aka cqxg CV 2020</p>
+        </div>
     </div>
-  </>
 );
 
 export default Footer;
