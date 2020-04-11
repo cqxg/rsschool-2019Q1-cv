7babb2cca2a79ca201521f9150634e9ad5716984
Ihar Ivaniuk <cqxg@yandex.ru>
2020-04-11 19:29:31 +0300

    feat: add ru/en btn

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { useTranslation, i18next } from 'react-i18next';
 
 import Navbar from './Navbar';
 
@@ -12,14 +12,14 @@ const Header = () => {
     return (
         <div id="header">
             <div className="header_logotype_container">
-                <h1 className="logotype_name">Ihar<span className="purple"> Ivaniuk</span></h1>
-                <h2 className="logotype_occupation">Web Developer</h2>
+                <h1 className="logotype_name">{t('fullName.name')}<span className="purple"> {t('fullName.surname')}</span></h1>
+                <h2 className="logotype_occupation">{t('currentPosition.position')}</h2>
             </div>
             <div className="header_menu_container">
-                {/* <div className='locale'>
-                        <div>RU</div>
-                        <div>EN</div>
-                    </div> */}
+                <div className='locale'>
+                    <div className='en' onClick={() => i18n.changeLanguage('en')}>EN</div>
+                    <div className='ru' onClick={() => i18n.changeLanguage('ru')}>RU</div>
+                </div>
                 <ul className="download_print_buttons horizontal_list">
                     <li><a href="./assets/Ihar_Ivaniuk_CV.pdf" download><span className="icon entypo-download" />Download CV</a></li>
                     <li onClick={() => goPrint()}><a className="print" href="#"><span className="icon entypo-print" />Print CV</a></li>
