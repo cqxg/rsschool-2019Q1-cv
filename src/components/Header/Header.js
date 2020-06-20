444844ae7aca465da7e6e022e14b854537986d97
unknown <cqxg@yandex.ru>
2020-06-20 19:59:23 +0300

    fix: href

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -50,7 +50,7 @@ const Header = () => {
                 </div>
                 <ul className="download_print_buttons horizontal_list">
                     <li><a href="./assets/Ihar_Ivaniuk_CV.pdf" download><img alt='dwnloadCV' className='download_print_icons' src={Download} />{t('dwnloadCV')}</a></li>
-                    <li onClick={() => goPrint()}><a className="print"><img alt='printCV' className="download_print_icons" src={Print} />{t('printCV')}</a></li>
+                    <li onClick={() => goPrint()}><a href='#' className="print"><img alt='printCV' className="download_print_icons" src={Print} />{t('printCV')}</a></li>
                 </ul>
                 <div className="clear" />
                 <Navbar />
