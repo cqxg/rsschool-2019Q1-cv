53612b4df4f8c754c4e25b6ee5492c2b2ef6c629
unknown <cqxg@yandex.ru>
2020-10-01 21:18:06 +0300

    feat: intelico exp update

--- src/components/Main/Profile.js
+++ src/components/Main/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import Intelico from '../../assets/jobScreens/Intelico.png';
+import Intelico from '../../assets/jobScreens/intelico.png';
 
 const Profile = () => {
     const { t } = useTranslation();
