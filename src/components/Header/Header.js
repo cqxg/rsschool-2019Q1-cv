846cf1631dbf05645c6ec5445efa2ddc2bb48264
unknown <cqxg@yandex.ru>
2020-12-09 23:02:48 +0300

    feat: add new i18n workpiece

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+import print from "../../assets/icons/print.svg";
+import download from "../../assets/icons/download.svg";
+
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
