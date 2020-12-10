2ec933b28b838d868998a05bde43f516c65ca992
unknown <cqxg@yandex.ru>
2020-12-10 19:35:48 +0300

    feat: add locales changing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
-import { Redirect } from "react-router";
-import { BrowserRouter, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { BrowserRouter } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 
-const App = () => (
-  <BrowserRouter>
-    <Header />
-  </BrowserRouter>
-);
+const App = () => {
+  const { t, i18n } = useTranslation();
+  const [activeLocale, setActiveLocale] = useState("en");
+
+  const changeLocale = (e) => setActiveLocale(e);
+
+  return (
+    <BrowserRouter>
+      <Header
+        t={t}
+        i18n={i18n}
+        changeLocale={changeLocale}
+        activeLocale={activeLocale}
+      />
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,46 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
-
-import Navbar from "../Navbar/Navbar";
 
 import printer from "../../assets/icons/print.svg";
 import downloader from "../../assets/icons/download.svg";
 
 import "./Header.scss";
 
-const Header = () => {
-  const { t, i18n } = useTranslation();
-
+const Header = ({ t, i18n, activeLocale, changeLocale }) => {
   const print = () => window.print();
 
-  return <header></header>;
+  const setLanguage = (language) => {
+    i18n.changeLanguage(language);
+    changeLocale(language);
+  };
+
+  return (
+    <header>
+      <div className="title-container">
+        <h1 className="title-container__name">
+          {t("name")}
+          <span className="title-container__surname"> {t("surname")}</span>
+        </h1>
+        <h2 className="title-container__position">{t("position")}</h2>
+      </div>
+      <div className="menu-container">
+        <div className="menu-container__locale">
+          <div
+            className={activeLocale === "en" ? "active" : ""}
+            onClick={() => setLanguage("en")}
+          >
+            EN
+          </div>
+          <div
+            className={activeLocale === "ru" ? "active" : ""}
+            onClick={() => setLanguage("ru")}
+          >
+            RU
+          </div>
+        </div>
+        <div className="clear" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
