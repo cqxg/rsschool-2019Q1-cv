9c40e182201d5b37b905be77d13b3386c94d73ad
unknown <cqxg@yandex.ru>
2020-12-15 12:18:41 +0300

    feat: work with start animation

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,97 +9,95 @@ import rss from "../../assets/screenshots/Work/rss.svg";
 
 import "./Profile.scss";
 
-const Profile = ({ t }) => {
-  return (
-    <div className="content-container">
-      <div className="content-container__block">
-        <h2>{t("summary")}</h2>
-        <blockquote>
-          <p>{t("summaryDesc")}</p>
-        </blockquote>
+const Profile = ({ t }) => (
+  <div className="content-container">
+    <div className="content-container__block">
+      <h2>{t("summary")}</h2>
+      <blockquote>
+        <p>{t("summaryDesc")}</p>
+      </blockquote>
+    </div>
+
+    <div className="horizontal-line">
+      <div className="line-left" />
+      <div className="left-circle" />
+      <div className="central-circle" />
+      <div className="right-circle" />
+      <div className="line-right" />
+    </div>
+
+    <div className="content-container__block">
+      <h2>{t("skills")}</h2>
+      <Cubes />
+
+      <h2>{t("anotherSkills")}</h2>
+      <div className="content-container__another">
+        <p>{t("research")}</p>
+        <Stars filledStars={4} emptyStars={1} />
       </div>
 
-      <div className="horizontal-line">
-        <div className="line-left" />
-        <div className="left-circle" />
-        <div className="central-circle" />
-        <div className="right-circle" />
-        <div className="line-right" />
+      <div className="content-container__another">
+        <p>{t("modeling")}</p>
+        <Stars filledStars={3} emptyStars={2} />
       </div>
 
-      <div className="content-container__block">
-        <h2>{t("skills")}</h2>
-        <Cubes />
-
-        <h2>{t("anotherSkills")}</h2>
-        <div className="content-container__another">
-          <p>{t("research")}</p>
-          <Stars filledStars={4} emptyStars={1} />
-        </div>
-
-        <div className="content-container__another">
-          <p>{t("modeling")}</p>
-          <Stars filledStars={3} emptyStars={2} />
-        </div>
-
-        <div className="content-container__another">
-          <p>{t("management")}</p>
-          <Stars filledStars={4} emptyStars={1} />
-        </div>
-
-        <div className="content-container__another">
-          <p>{t("communication")}</p>
-          <Stars filledStars={4} emptyStars={1} />
-        </div>
-
-        <div className="content-container__another">
-          <p>{t("processes")}</p>
-          <Stars filledStars={3} emptyStars={2} />
-        </div>
+      <div className="content-container__another">
+        <p>{t("management")}</p>
+        <Stars filledStars={4} emptyStars={1} />
       </div>
 
-      <div className="horizontal-line">
-        <div className="line-left" />
-        <div className="left-circle" />
-        <div className="central-circle" />
-        <div className="right-circle" />
-        <div className="line-right" />
+      <div className="content-container__another">
+        <p>{t("communication")}</p>
+        <Stars filledStars={4} emptyStars={1} />
       </div>
 
-      <div className="content-container__block">
-        <div className="content-container__experience">
-          <h2>{t("experience")}</h2>
-          <h3>2015 - 2018</h3>
-          <p>{t("experience2015p1")}</p>
-          <p>{t("experience2015p2")}</p>
-          <p>{t("experience2015p3")}</p>
-          <p>{t("experience2015p4")}</p>
-
-          <h3>2019 - 2020</h3>
-          <a
-            href="https://en.wikipedia.org/wiki/Freelancer"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={freelance} alt="freelance" />
-          </a>
-          <p>{t("experience2019p1")}</p>
-
-          <h3>2020 - Present</h3>
-          <a href="https://intelico.pro/" target="_blank" rel="noreferrer">
-            <img src={intelico} alt="intelico" />
-          </a>
-          <p>{t("experienceIntelico")}</p>
-
-          <a href="https://rs.school/" target="_blank" rel="noreferrer">
-            <img src={rss} alt="rss" />
-          </a>
-          <p>{t("experienceRss")}</p>
-          <p>{t("ownExperience")}</p>
-        </div>
+      <div className="content-container__another">
+        <p>{t("processes")}</p>
+        <Stars filledStars={3} emptyStars={2} />
       </div>
     </div>
-  );
-};
+
+    <div className="horizontal-line">
+      <div className="line-left" />
+      <div className="left-circle" />
+      <div className="central-circle" />
+      <div className="right-circle" />
+      <div className="line-right" />
+    </div>
+
+    <div className="content-container__block">
+      <div className="content-container__experience">
+        <h2>{t("experience")}</h2>
+        <h3>2015 - 2018</h3>
+        <p>{t("experience2015p1")}</p>
+        <p>{t("experience2015p2")}</p>
+        <p>{t("experience2015p3")}</p>
+        <p>{t("experience2015p4")}</p>
+
+        <h3>2019 - 2020</h3>
+        <a
+          href="https://en.wikipedia.org/wiki/Freelancer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={freelance} alt="freelance" />
+        </a>
+        <p>{t("experience2019p1")}</p>
+
+        <h3>2020 - Present</h3>
+        <a href="https://intelico.pro/" target="_blank" rel="noreferrer">
+          <img src={intelico} alt="intelico" />
+        </a>
+        <p>{t("experienceIntelico")}</p>
+
+        <a href="https://rs.school/" target="_blank" rel="noreferrer">
+          <img src={rss} alt="rss" />
+        </a>
+        <p>{t("experienceRss")}</p>
+        <p>{t("ownExperience")}</p>
+      </div>
+    </div>
+  </div>
+);
 
 export default Profile;
