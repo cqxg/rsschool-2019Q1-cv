540105bf1f8518ceed2be50cfe83ac6a3b7af592
unknown <cqxg@yandex.ru>
2020-12-15 23:54:01 +0300

    feat: add notify checkbox lightning

--- src/components/Projects/Notify/Notify.js
+++ src/components/Projects/Notify/Notify.js
@@ -1,28 +1,26 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import "./Notify.scss";
 
-const Notify = ({ t }) => {
-  return (
-    <div className="notify">
-      <input type="checkbox" />
-      <p>
-        {t("notyEnc")}
-        <a
-          className="education_link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/cqxg"
-        >
-          {" "}
-          github
-        </a>
-        !
-        <br />
-        {t("notyDesc")}
-      </p>
-    </div>
-  );
-};
+const Notify = ({ t }) => (
+  <div className="notify">
+    <input type="checkbox" />
+    <p>
+      {t("notyEnc")}
+      <a
+        className="education_link"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/cqxg"
+      >
+        {" "}
+        github
+      </a>
+      !
+      <br />
+      {t("notyDesc")}
+    </p>
+  </div>
+);
 
 export default Notify;
