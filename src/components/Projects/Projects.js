13d07ea1ff5340cf85897f498ec92bac9269b77f
unknown <cqxg@yandex.ru>
2020-12-17 00:33:06 +0300

    feat: add notify flashing

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import Joke from "./Joke/Joke";
 import Notify from "./Notify/Notify";
 
-import plat from "../../assets/screenshots/Work/plat.svg";
+import plat from "../../assets/screenshots/Projects/plat.svg";
 
 import "./Projects.scss";
 
@@ -26,17 +26,15 @@ const Projects = ({ t }) => {
         <Notify t={t} />
         <h2>{t("projects")}</h2>
 
-        <h4>
-          Plat | Lead Distribution Platform (Feb 2020 - Present)
-        </h4>
+        <h4>Plat | Lead Distribution Platform (Feb 2020 - Present)</h4>
         <a href="https://dot818.com/plat/" target="_blank" rel="noreferrer">
-          <img className="border" src={plat} alt="Plat" />
+          <img src={plat} alt="Plat" />
         </a>
         <p>{t("platDesc")}</p>
         <p>{t("platMoreDesc")}</p>
 
-        <div className="env_wrap">
-          <p className="highlight">{t("env")}</p>
+        <div>
+          <p>{t("env")}</p>
           <p>JavaScript, React, Redux, Bootstrap, SCSS etc.</p>
         </div>
       </div>
